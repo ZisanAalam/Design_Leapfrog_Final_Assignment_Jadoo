3a4535094c8256f6c68042ad80a3c662590e640b
ZisanAalam <47499198+ZisanAalam@users.noreply.github.com>
2021-12-17 10:51:58 +0545

    dropwdown trnasition and bg-color added

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const navItems = document.querySelector(".navbar-items");
+const navbar = document.querySelector(".navbar");
 
 
 hamburger.addEventListener('click',() => {
     navItems.classList.toggle('show');
+    navbar.classList.toggle('bg-color');
 });
